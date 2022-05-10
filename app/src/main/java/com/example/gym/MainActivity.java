35b262abaaa43c71218d40f1d1b9052e27cf960d
SrSpikii <wilson9579@gmail.com>
2022-05-10 17:30:31 +0200

    pantalla de carga

--- app/src/main/java/com/example/gym/MainActivity.java
+++ app/src/main/java/com/example/gym/MainActivity.java
@@ -1,14 +1,55 @@
 package com.example.gym;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.gym.ui.login.LoginActivity;
+import com.lusfold.spinnerloading.SpinnerLoading;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sqlThread.start();
+        intent();
     }
+
+    public void intent(){
+
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    Thread sqlThread = new Thread() {
+        public void run() {
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+                // "jdbc:mysql://IP:PUERTO/DB", "USER", "PASSWORD");
+                // Si estás utilizando el emulador de android y tenes el mysql en tu misma PC no utilizar 127.0.0.1 o localhost como IP, utilizar 10.0.2.2
+                Connection conn = DriverManager.getConnection(
+                        "jdbc:mysql://192.168.0.4:3306/fifa", "test", "test");
+                //En el stsql se puede agregar cualquier consulta SQL deseada.
+                String stsql = "Select version()";
+                Statement st = conn.createStatement();
+                ResultSet rs = st.executeQuery(stsql);
+                rs.next();
+                System.out.println( rs.getString(1) );
+                conn.close();
+            } catch (SQLException se) {
+                System.out.println("oops! No se puede conectar. Error: " + se.toString());
+            } catch (ClassNotFoundException e) {
+                System.out.println("oops! No se encuentra la clase. Error: " + e.getMessage());
+            }
+        }
+    };
 }
--- app/src/main/java/com/example/gym/SplashScreen.java
+++ app/src/main/java/com/example/gym/SplashScreen.java
@@ -6,12 +6,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.lusfold.spinnerloading.SpinnerLoading;
+
 public class SplashScreen extends AppCompatActivity {
+
+    SpinnerLoading spinnerLoading;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        spinnerLoading = findViewById(R.id.spinnerloading);
 
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -20,6 +26,6 @@ public class SplashScreen extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        },2000);
+        },4000);
     }
 }
