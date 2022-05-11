2092a938f4946a12a0ec2d61f33878a4147be571
fxop0218 <oliverasfrancescins@gmail.com>
2022-05-11 19:32:28 +0200

    Add CalendarFragment Add calendar to CalendarFragment.java and 2 buttons Add css to buttons and the fking calendar

--- app/src/main/java/com/example/gym/CalendarFragment.java
+++ app/src/main/java/com/example/gym/CalendarFragment.java
@@ -1,12 +1,21 @@
 package com.example.gym;
 
+import android.app.usage.UsageEvents;
+import android.graphics.Color;
+import android.media.metrics.Event;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
+import android.widget.Toast;
+
+import com.google.android.material.tabs.TabLayout;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,10 +28,12 @@ public class CalendarFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private CalendarView cvCalendar;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private String date;
 
     public CalendarFragment() {
         // Required empty public constructor
@@ -36,6 +47,7 @@ public class CalendarFragment extends Fragment {
      * @param param2 Parameter 2.
      * @return A new instance of fragment CalendarFragment.
      */
+
     // TODO: Rename and change types and number of parameters
     public static CalendarFragment newInstance(String param1, String param2) {
         CalendarFragment fragment = new CalendarFragment();
@@ -52,13 +64,36 @@ public class CalendarFragment extends Fragment {
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
+
         }
     }
 
+    /**
+     * Inflate the XML layout for the Fragment in this callback.
+     * Create a onSelectDayChanged() to get the day selected by the user
+     *
+     * @param inflater
+     * @param container
+     * @param savedInstanceState
+     *
+     * @return view
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_calendar, container, false);
+        View view = inflater.inflate(R.layout.fragment_calendar, container, false);
+        cvCalendar = (CalendarView) view.findViewById(R.id.cvCalendar);
+        cvCalendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView calendarView, int i, int i1, int i2) {
+                date = i + "/" + i1 + "/" +i2;
+                Log.d("CalendarFragment", "onSelectedDayChange: date:"+ date);
+                Toast.makeText(getContext(), date, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        return view;
     }
 }
