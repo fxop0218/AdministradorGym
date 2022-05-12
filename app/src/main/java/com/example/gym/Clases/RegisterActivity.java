28cce26b2307015b7357e4ecffef8de019416def
fxop0218 <oliverasfrancescins@gmail.com>
2022-05-12 20:12:42 +0200

    Se añade edit text personalizados en activity_login.xml Ahora el dni tiene que ser valido y existente para poder añadirlo en RegisterActivity.java

--- app/src/main/java/com/example/gym/Clases/RegisterActivity.java
+++ app/src/main/java/com/example/gym/Clases/RegisterActivity.java
@@ -48,7 +48,7 @@ public class RegisterActivity extends Activity {
 
             @Override
             public void afterTextChanged(Editable editable) {
-                if (dni_validator(etDni)) {
+                if (!dni_validator()) {
                     etDni.setError("El dni tiene que contener 9 caracteres");
                     bRegister.setEnabled(setRegisterEnabled());
                 }
@@ -223,40 +223,60 @@ public class RegisterActivity extends Activity {
         return false;
     }
 
-    private boolean dni_validator(EditText etDni) {
+    private boolean dni_validator() {
         String dni = etDni.getText().toString();
         String mayusLetter = "";
-        if (is_not_correct(etDni, 9) || Character.isLetter(dni.charAt(8)) == false)
+        if (dni.length() != 9 || !Character.isLetter(dni.charAt(8))) {
             return false;
+        }
         mayusLetter = (dni.substring(8)).toUpperCase();
 
-        if (onlyNumbers()) {
+        if (onlyNumbers(dni) && letraDNI().equals(mayusLetter)) {
             return true;
         } else {
             return false;
         }
     }
 
-    private boolean onlyNumbers(){
+    /**
+     *
+     * Mira si el dni introducido por el usuario es correcto
+     * y es un dni existente, es decir, que la letra coincide con
+     * el calculo de los numeros anteriores
+     *
+     *
+     * @param dni
+     * @return Si el dni es correcto devuelve true, si no lo es false
+     */
+    private boolean onlyNumbers(String dni){
         int i,j=0;
-        String dni = etDni.getText().toString();
         String numero="";
         String miDNI="";
         String[] unoNueve={"0","1","2","3","4","5","6","7","8","9"};
-        for(i=0;i< dni. length()-1; i++){
-            numero= dni. substring(i, i+1);
+        for(i=0;i< dni.length()-1; i++) {
+            numero = dni.substring(i, i + 1);
 
-            for(j=0;j< unoNueve. length; j++){
-                if(numero. equals(unoNueve [j])){
+            for (j = 0; j < unoNueve.length; j++) {
+                if (numero.equals(unoNueve[j])) {
                     miDNI += unoNueve[j];
-                }
-                if(miDNI.length() != 8){
-                    return false;
-                }
-                else{
-                    return true;
                 }
             }
         }
+        if(miDNI.length() != 8){
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private String letraDNI(){
+        String dni = etDni.getText().toString();
+        int miDNI=Integer.parseInt(dni.substring(0,8));
+        int resto=0;
+        String miletra="";
+        String[] asignacionletra = {"T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X","B","N", "J", "Z","S","Q","V", "H", "L", "C", "K","E"};
+        resto = miDNI % 23;
+        miletra = asignacionletra[resto];
+        return miletra;
     }
 }
